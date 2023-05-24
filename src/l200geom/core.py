18e95effc195c3d3a653d87a518398be9eaa58af
Manuel Huber <info@manuelhu.de>
2023-05-24 13:22:36 +0200

    add lazily defined materials for optics

--- src/l200geom/core.py
+++ src/l200geom/core.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 from pyg4ometry import geant4
 
 from . import cryo
+from . import materials
 
 
 def construct() -> geant4.Registry:
     """Construct the LEGEND-200 geometry and return the pyg4ometry Registry containing the world volume."""
 
     reg = geant4.Registry()
+    mats = materials.OpticalMaterialRegistry(reg)
 
     # Create the world volume
     world_material = geant4.MaterialPredefined('G4_Galactic')
@@ -20,10 +22,10 @@ def construct() -> geant4.Registry:
     coordinate_z_displacement = 0
 
     # Create basic structure with argon and cryostat.
-    cryostat_lv = cryo.construct_cryostat(world_material, reg)
+    cryostat_lv = cryo.construct_cryostat(mats.metal_steel, reg)
     cryostat_pv = cryo.place_cryostat(cryostat_lv, world_lv, coordinate_z_displacement, reg)
 
-    lar_lv = cryo.construct_argon(world_material, reg)
+    lar_lv = cryo.construct_argon(mats.liquidargon, reg)
     lar_pv = cryo.place_argon(lar_lv, cryostat_lv, coordinate_z_displacement, reg)
 
     return reg
