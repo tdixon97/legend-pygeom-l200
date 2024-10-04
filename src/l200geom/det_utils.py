27c4ed4adfc05b525460fabc53a189273a6c04ca
Manuel Huber <info@manuelhu.de>
2024-10-05 00:27:27 +0200

    test expected sensitive detector counts

--- src/l200geom/det_utils.py
+++ src/l200geom/det_utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from collections.abc import Generator
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
@@ -16,23 +17,40 @@ class RemageDetectorInfo:
     uid: int
 
 
-def _detector_macro_recursive(pv: g4.PhysicalVolume, macro_lines: dict) -> None:
-    if hasattr(pv, "pygeom_active_dector") and pv.name not in macro_lines:
+def walk_detectors(
+    pv: g4.PhysicalVolume | g4.LogicalVolume | g4.Registry,
+) -> Generator[tuple[g4.PhysicalVolume, RemageDetectorInfo], None, None]:
+    if isinstance(pv, g4.PhysicalVolume) and hasattr(pv, "pygeom_active_dector"):
         det = pv.pygeom_active_dector
         assert isinstance(det, RemageDetectorInfo)
-        mac = f"/RMG/Geometry/RegisterDetector {det.detector_type.title()} {pv.name} {det.uid}\n"
-        macro_lines[pv.name] = mac
+        yield pv, det
 
-    for dv in pv.logicalVolume.daughterVolumes:
-        if pv.type == "placement":
-            _detector_macro_recursive(dv, macro_lines)
+    if isinstance(pv, g4.LogicalVolume):
+        next_v = pv
+    if isinstance(pv, g4.PhysicalVolume):
+        next_v = pv.logicalVolume
+    elif isinstance(pv, g4.Registry):
+        next_v = pv.worldVolume
+    else:
+        msg = "invalid type encountered in walk_detectors volume tree"
+        raise TypeError(msg)
+
+    for dv in next_v.daughterVolumes:
+        if dv.type == "placement":
+            yield from walk_detectors(dv)
 
 
 def generate_detector_macro(registry: g4.Registry, filename: str) -> None:
     """Create a Geant4 macro file containing the defined active detector volumes for use in remage."""
+
     macro_lines = {}
-    for pv in registry.worldVolume.daughterVolumes:
-        _detector_macro_recursive(pv, macro_lines)
+
+    for pv, det in walk_detectors(registry):
+        if pv.name in macro_lines:
+            return
+        mac = f"/RMG/Geometry/RegisterDetector {det.detector_type.title()} {pv.name} {det.uid}\n"
+        macro_lines[pv.name] = mac
+
     macro_contents = "".join(macro_lines.values())
 
     with Path.open(filename, "w", encoding="utf-8") as f:
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from collections import Counter
+from pathlib import Path
+
 import numpy as np
 
 
@@ -7,11 +10,27 @@ def test_import():
     import l200geom  # noqa: F401
 
 
-def test_construct():
-    from l200geom import core
+def test_construct(tmp_path):
+    from l200geom import core, det_utils
 
-    core.construct(use_detailed_fiber_model=False)
-    core.construct(use_detailed_fiber_model=True)
+    registry = core.construct(use_detailed_fiber_model=False)
+    # verify that we get the expected channel counts.
+    ch_count = Counter([d.detector_type for f, d in det_utils.walk_detectors(registry)])
+    assert ch_count["optical"] == 2 * (9 + 20)  # 2*(IB+OB)
+    assert ch_count["germanium"] == 101  # from channelmap @ 20230311T235840Z
+    det_file_detailed = tmp_path / "det-detailed.mac"
+    det_utils.generate_detector_macro(registry, det_file_detailed)
+
+    registry = core.construct(use_detailed_fiber_model=True)
+    # verify that we get the expected channel counts.
+    ch_count = Counter([d.detector_type for f, d in det_utils.walk_detectors(registry)])
+    assert ch_count["optical"] == 2 * (9 + 20)  # 2*(IB+OB)
+    assert ch_count["germanium"] == 101  # from channelmap @ 20230311T235840Z
+    det_file_segmented = tmp_path / "det-segmented.mac"
+    det_utils.generate_detector_macro(registry, det_file_segmented)
+
+    with Path.open(det_file_detailed) as f_det, Path.open(det_file_segmented) as f_seg:
+        assert f_det.readlines() == f_seg.readlines()
 
 
 def test_material_store():
