2c0e8614111d4e0e91789683f83470c20bc2abc3
Manuel Huber <ManuelHu@users.noreply.github.com>
2024-12-12 16:58:13 +0100

    tests: more fixes after public geometry

--- tests/test_core.py
+++ tests/test_core.py
@@ -16,29 +16,42 @@ def test_import():
     import l200geom  # noqa: F401
 
 
+@pytest.fixture(scope="session")
+def conctruct_fiber_variants():
+    from l200geom import core
+
+    reg_detailed = core.construct(use_detailed_fiber_model=True, public_geometry=public_geom)
+    reg_segmented = core.construct(use_detailed_fiber_model=False, public_geometry=public_geom)
+
+    return reg_detailed, reg_segmented
+
+
+def test_construct(tmp_path, conctruct_fiber_variants):
+    # do nothing here, we just want to test the fixture.
+    reg_detailed, reg_segmented = conctruct_fiber_variants
+
+
 @pytest.mark.xfail(
     public_geom,
     reason="public geometry has different detector count",
     raises=AssertionError,
 )
-def test_construct(tmp_path):
-    from l200geom import core
+def test_detector_count(tmp_path, conctruct_fiber_variants):
+    reg_detailed, reg_segmented = conctruct_fiber_variants
 
-    registry = core.construct(use_detailed_fiber_model=True, public_geometry=public_geom)
     # verify that we get the expected channel counts.
-    ch_count = Counter([d.detector_type for f, d in detectors.walk_detectors(registry)])
+    ch_count = Counter([d.detector_type for f, d in detectors.walk_detectors(reg_detailed)])
     assert ch_count["optical"] == 2 * (9 + 20)  # 2*(IB+OB)
     assert ch_count["germanium"] == 101  # from channelmap @ 20230311T235840Z
     det_file_detailed = tmp_path / "det-detailed.mac"
-    detectors.generate_detector_macro(registry, det_file_detailed, public_geometry=public_geom)
+    detectors.generate_detector_macro(reg_detailed, det_file_detailed)
 
-    registry = core.construct(use_detailed_fiber_model=False)
     # verify that we get the expected channel counts.
-    ch_count = Counter([d.detector_type for f, d in detectors.walk_detectors(registry)])
+    ch_count = Counter([d.detector_type for f, d in detectors.walk_detectors(reg_segmented)])
     assert ch_count["optical"] == 2 * (9 + 20)  # 2*(IB+OB)
     assert ch_count["germanium"] == 101  # from channelmap @ 20230311T235840Z
     det_file_segmented = tmp_path / "det-segmented.mac"
-    detectors.generate_detector_macro(registry, det_file_segmented)
+    detectors.generate_detector_macro(reg_segmented, det_file_segmented)
 
     with (
         Path(det_file_detailed).open(encoding="utf-8") as f_det,
@@ -47,14 +60,13 @@ def test_construct(tmp_path):
         assert f_det.readlines() == f_seg.readlines()
 
 
-def test_read_back(tmp_path):
-    from l200geom import core
+def test_read_back(tmp_path, conctruct_fiber_variants):
+    _, reg_segmented = conctruct_fiber_variants
 
-    registry = core.construct(use_detailed_fiber_model=False, public_geometry=public_geom)
     # write a GDML file.
     gdml_file_detailed = tmp_path / "segmented.gdml"
     w = gdml.Writer()
-    w.addDetector(registry)
+    w.addDetector(reg_segmented)
     w.write(gdml_file_detailed)
     # try to read it back.
     gdml.Reader(gdml_file_detailed)
@@ -72,12 +84,20 @@ def test_material_store():
 
     # test that replaced material properties are reflected in the GDML.
     fiber_core_refractive_index.replace_implementation(lambda: 1234)
-    reg = core.construct(use_detailed_fiber_model=False, public_geometry=public_geom)
+    reg = core.construct(
+        use_detailed_fiber_model=False,
+        assemblies=["fibers"],
+        public_geometry=public_geom,
+    )
     rindex = reg.defineDict["ps_fibers_RINDEX"].eval()
     assert np.all(rindex[:, 1] == [1234, 1234])
 
     # test that after the reset, the created GDML contains the original values again.
     store.reset_all_to_original()
-    reg = core.construct(use_detailed_fiber_model=False, public_geometry=public_geom)
+    reg = core.construct(
+        use_detailed_fiber_model=False,
+        assemblies=["fibers"],
+        public_geometry=public_geom,
+    )
     rindex = reg.defineDict["ps_fibers_RINDEX"].eval()
     assert np.all(rindex[:, 1] == [1.6, 1.6])
